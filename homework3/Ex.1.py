d46c261c429fa55fb27df081cf7c0e8fc7525315
Multistime <multistime@gmail.com>
2018-11-13 00:16:45 +0300

    Merge branch 'master' of github.com:multistime/homeworks
    # Conflicts:
    #	modules homework/string_module.py

--- homework3/Ex.1.py
+++ homework3/Ex.1.py
@@ -1,7 +1,8 @@
 import random
 from functools import reduce
 
-N = int(input('Введите количество элементов в списке:')) #вводим цифру для определения количества случайных цифр
+N = int(input('Введите количество элементов в списке:'))
+#вводим цифру для определения количества случайных цифр
 
 numbers = []
 
@@ -10,6 +11,12 @@ while len(numbers)<N:
     numbers.append(x)
 
 print(numbers)
+
+action = input("Что будем делать?:")
+
+action_split = action.split()
+
+print(action_split)
 
 #1 слово
 
@@ -22,8 +29,6 @@ union = list(set(numbers))
 join = reduce(lambda res, x: 10*res + x, numbers)
 
 reverse = reduce(lambda res, x: [x] + res, numbers, [])
-
-print(union)
 
 #2 слово
 
@@ -41,30 +46,12 @@ evens = list(filter(lambda x: x % 2 == 0, numbers))
 
 #simples = list(filter(lambda x: x ==, numbers))
 
-print(odds,evens)
-
 func_dict = {"sum":sum,"multiply":multiply,
              "union":union,"join":join,
              "reverse":reverse,"negated":negated,
              "inverted":inverted,"squared":squared,
              "odds":odds,"evens":evens}
 
-action = input("Что будем делать?:")
-
-action_split = action.split()
-
-print(action_split)
-
 x = action_split[0]
 y = action_split[1]
 z = action_split[2]
-
-func_dict.get(z)
-
-print(x, y, z)
-
-list_a = func_dict.get(z)
-
-list_b = func_dict.get(y)
-
-list_c = func_dict.get(x)
